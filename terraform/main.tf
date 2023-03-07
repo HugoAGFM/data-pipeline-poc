69b7bc3829daffd7d2455bd34b6da5a754c9cd70
Hugo Mendonça <96540348+HugoAGFM@users.noreply.github.com>
2023-03-07 16:50:59 -0300

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatehugoagfm"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+# Provider
+provider "azurerm" {
+  features {}
+}
+
+
+# Resource Groups
+resource "azurerm_resource_group" "data_pipeline_rg" {
+  name     = "data-pipeline-rg"
+  location = "East US 2"
+}
+
+
+# Service Plans
+resource "azurerm_service_plan" "data_pipeline_sp" {
+  name                = "data-pipeline-sp"
+  resource_group_name = azurerm_resource_group.data_pipeline_rg.name
+  location            = azurerm_resource_group.data_pipeline_rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+
+# Storage Accounts
+resource "azurerm_storage_account" "data_pipeline_sa" {
+  name                     = "datapipelinesa"
+  resource_group_name      = azurerm_resource_group.data_pipeline_rg.name
+  location                 = azurerm_resource_group.data_pipeline_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+# Containers
+resource "azurerm_storage_container" "data_piepline_staging" {
+  name                  = "staging"
+  storage_account_name  = azurerm_storage_account.data_pipeline_sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "data_piepline_validated" {
+  name                  = "validated"
+  storage_account_name  = azurerm_storage_account.data_pipeline_sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "data_piepline_processed" {
+  name                  = "processed"
+  storage_account_name  = azurerm_storage_account.data_pipeline_sa.name
+  container_access_type = "private"
+}
+
+
+# Function Apps
+resource "azurerm_linux_function_app" "data_pipeline_linux_fa" {
+  name                = "data-pipeline-linux-fa"
+  resource_group_name = azurerm_resource_group.data_pipeline_rg.name
+  location            = azurerm_resource_group.data_pipeline_rg.location
+
+  storage_account_name       = azurerm_storage_account.data_pipeline_sa.name
+  storage_account_access_key = azurerm_storage_account.data_pipeline_sa.primary_access_key
+
+  service_plan_id = azurerm_service_plan.data_pipeline_sp.id
+
+#   app_settings = {
+#     "username" = "hugoagfm",
+#     "password" = "123456"
+#   }
+  
+  site_config {}
+}
+
+# Key Vault
